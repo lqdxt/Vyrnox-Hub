a15c8c21b40492805b08b39e685e37e8d3b06664
lqdxt <nothingidk793@gmail.com>
2026-08-29 14:18:18 -0600

    Add header comment for script usage instructions
    Added a header comment explaining script usage and restrictions.

--- The Rake REMASTERED.lua
+++ The Rake REMASTERED.lua
@@ -1,1 +1,5 @@
-
+--[[
+ for The Rake REMASTERED
+ any executions in other games might get you kicked as it's not an universal script
+]]
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/b3eb88f529415bf4ba11197f32d533a0c8dc6dcb906154978e9f420de39f345e/download"))()
